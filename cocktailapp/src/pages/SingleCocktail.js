ad0820aae7e230ead74a4d53216d50057d366473
HAFSA BAJWA <hafsa10117@gmail.com>
2022-10-21 10:12:31 +0200

    Router setup created

---
+++ cocktailapp/src/pages/SingleCocktail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SingleCocktail = () => {
+  return (
+    <div>Single Cocktail</div>
+  )
+}
+
+export default SingleCocktail
---
+++ cocktailapp/src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <h2>Home</h2>
+        {/* Search input */}
+        {/* Cocktail list */}
+    </div>
+  )
+}
+
+export default Home
