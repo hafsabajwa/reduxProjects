ab85338ddc5187c6c31444214bd84e6c16f7d389
HAFSA BAJWA <hafsa10117@gmail.com>
2022-10-21 10:27:18 +0200

    add css code

--- cocktailapp/src/components/Header.js
+++ cocktailapp/src/components/Header.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 export const Header = () => {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <nav className='navbar navbar-light bg-light fixed-top'>
+            <div className='container-fluid'>
+                <span className='navbar-brand mb-0 h1 header-title text-success'>
+                    Find Your Favourite Cocktail
+                </span>
+            </div>
+        </nav>
+    )
 }
 
