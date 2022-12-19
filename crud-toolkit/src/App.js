9a40b71067bc35cbc26f8523dacb20703a40afba
HAFSA BAJWA <hafsa10117@gmail.com>
2022-12-19 14:03:59 +0100

    changes added

--- crud-toolkit/src/App.js
+++ crud-toolkit/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import {Route, Routes, BrowserRouter} from "react-router-dom"
 import Home from './UserPost'
+import CreatePost from './UserPost/CreatePost';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/createPost" element={<CreatePost />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
