e6af4c6e2638872c34b4a46cf23f3524efff2f29
HAFSA BAJWA <hafsa10117@gmail.com>
2022-12-20 14:43:26 +0100

    changes added

--- crud-toolkit/src/redux/features/postSlice.js
+++ crud-toolkit/src/redux/features/postSlice.js
@@ -15,6 +15,23 @@ export const deletePost = createAsyncThunk(
   }
 );
 
+export const createPost = createAsyncThunk(
+  "post/createPost",
+  async ({ values }) => {
+    return fetch(`https://jsonplaceholder.typicode.com/posts/`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        title: values.title,
+        body: values.body,
+      }),
+    }).then((res) => res.json());
+  }
+);
+
 const postSlice = createSlice({
   name: "post",
   initialState: {
@@ -42,6 +59,17 @@ const postSlice = createSlice({
       state.post = action.payload;
     },
     [deletePost.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
+    [createPost.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [createPost.fulfilled]: (state, action) => {
+      state.loading = false;
+      state.post = [action.payload];
+    },
+    [createPost.rejected]: (state, action) => {
       state.loading = false;
       state.error = action.payload;
     },
