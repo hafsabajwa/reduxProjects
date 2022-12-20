a71d7788763afd2f9e00dddbefeb2fa34e17556c
HAFSA BAJWA <hafsa10117@gmail.com>
2022-12-20 14:59:40 +0100

    changes added

--- crud-toolkit/src/UserPost/CreatePost.js
+++ crud-toolkit/src/UserPost/CreatePost.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Button, Input, Space } from "antd";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
 const CreatePost = () => {
+  const [values, setValues] = useState({ title: "", body: "" });
+  const [showPost, setShowPost] = useState(false);
+  const { title, body } = values;
+  const dispatch = useDispatch();
+  const handleSubmit = (e) => {};
   return (
-    <h1>CreatePost</h1>
-  )
-}
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>CreatePost</h1>
+        <Input
+          placeholder="Enter Title"
+          type="text"
+          onChange={(e) => setValues({ ...values, title: e.target.value })}
+          value={title}
+          style={{ width: "400px" }}
+        />
+        <br /> <br />
+        <Input.TextArea
+          placeholder="Enter body"
+          type="text"
+          onChange={(e) => setValues({ ...values, body: e.target.value })}
+          value={body}
+          style={{ width: "400px" }}
+          size="large"
+        />
+        <br /> <br />
+        <Space style={{margin:10}}>
+          <Button>Go Back</Button>
+          <Button type="primary" htmlType="submit">Submit</Button>
+        </Space>
+      </form>
+    </div>
+  );
+};
 
-export default CreatePost
+export default CreatePost;
